e4585d1a7193e224715e090980fa34035eaf3b4d
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2023-09-21 21:02:11 -0500

    initial commit

---
+++ connectwave-backend/src/main/java/com/abdulmo123/connectwave/ConnectwaveApplication.java
@@ -0,0 +1,13 @@
+package com.abdulmo123.connectwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectwaveApplication.class, args);
+	}
+
+}
---
+++ connectwave-backend/src/test/java/com/abdulmo123/connectwave/ConnectwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulmo123.connectwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
