4dc223a6c23010b796d5714d349928b2b1662256
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2023-10-19 20:20:27 -0500

    user repository setup

---
+++ connectwave-backend/src/main/java/com/abdulmo123/connectwave/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.abdulmo123.connectwave.repository;
+
+import com.abdulmo123.connectwave.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<Long, User> {
+
+    Optional<User> findByEmail(String email);
+}
